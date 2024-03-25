6133483423845813025dd1efea8f375e6463ebcc
WYC1000 <110243018+WYC1000@users.noreply.github.com>
2024-03-25 11:35:00 +0800

    home&gept

---
+++ main.js
@@ -0,0 +1,23 @@
+//change image by clicking
+// var myImage = document.querySelector("img");
+// myImage.onclick = function () {
+//   let mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/intro-2.jpg") {
+//     myImage.setAttribute("src", "images/intro.jpg");
+//   } 
+//   else if(mySrc === "images/intro.jpg"){
+//     myImage.setAttribute("src", "images/intro-2.jpg");
+//   }
+// };
+
+//
+window.onload = function(){
+  const element = document.getElementById('js-btn_home');
+  element.addEventListener('click', foo, false);
+};
+function foo(){
+  
+}
+  
+
+
